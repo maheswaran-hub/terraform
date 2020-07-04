f60a9dbede9e95893dba9a90300ee30ec949d7cc
Pa <maheswaran.pa@accenture.com>
2020-07-04 06:54:55 +0530

    Adding Script for Application gateway

---
+++ APPWG.ps1
@@ -0,0 +1,12 @@
+﻿#Login:
+#Login-AzureRmAccount
+#Get-AzureRmSubscription |Out-GridView -PassThru | Select-AzureRmSubscription
+
+#GET Details
+#Get-AzureRmApplicationGateway | ? {$_.Name -like "*AGW*"} |Select-Object -Property  Name,OperationalState
+
+
+#Start/stop:
+Get-AzureRmApplicationGateway | ? {$_.Name -like "*AGW-WETS*"} |foreach {start-AzureRmApplicationGateway -ApplicationGateway $_}
+Get-AzureRmApplicationGateway | ? {$_.Name -like "*AGW-WEQA*"} |foreach {stop-AzureRmApplicationGateway -ApplicationGateway $_}
+
